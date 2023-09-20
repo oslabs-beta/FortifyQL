0a560465bf940d20561a3c898059bd760aca218c
Rachel Power <96928310+rpower15@users.noreply.github.com>
2023-09-20 13:44:00 -0600

    Added database, resolvers, and schema placeholders. Updated package.json to include cors and graphql dependencies. Changed tsconfig Linting parameters temporarily to false. Changed app to server in server.ts.

--- server/server.ts
+++ server/server.ts
@@ -1,39 +1,40 @@
-import express, { Request, Response, NextFunction} from 'express'
+import express, { Request, Response, NextFunction } from 'express';
 // import path from 'path'
-import cors from 'cors'
+import cors from 'cors';
 
-const app = express();
+const server = express();
 const PORT = 3000;
 
 // REQUIRED ROUTES
 
 // Use cors
-app.use(cors());
+server.use(cors());
 
 // PARSE JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
 // GLOBAL ERROR HANDLER
 interface CustomError {
-    log?: string;
-    status?: number;
-    message?: {
-      err: string;
-    };
-  }
+  log?: string;
+  status?: number;
+  message?: {
+    err: string;
+  };
+}
 
-app.use((err: CustomError, req: Request, res: Response, _next: NextFunction) => {
+server.use(
+  (err: CustomError, req: Request, res: Response, _next: NextFunction) => {
     const defaultErr = {
       log: 'Express error handler caught unknown middleware error',
       status: 500,
       message: { err: 'An error occurred' },
     };
-  
+
     const errorObj = { ...defaultErr, ...err };
     console.log(errorObj.log);
     return res.status(errorObj.status).json(errorObj.message);
-  });
-  
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-  
+  },
+);
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
