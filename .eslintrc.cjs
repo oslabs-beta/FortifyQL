4dcbc9a12ced819672a1fcc971d7ccb8b906f97e
David Yoon <DYoonity@users.noreply.github.com>
2023-09-20 20:19:10 -0400

    prettier setup and config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
