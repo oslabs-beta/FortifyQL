9cb55c68f5f69a64f9b104427b3d73aa2d44689a
aydenyipcs <aydenyip56@gmail.com>
2023-09-20 21:24:14 -0400

    route for tests, middleware to get Schema from endpoint, some configuration files for ts compaibility and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','react','import'],
+  parserOptions: {
+    project: './tsconfig.json', //can delete later not sure what it does 
+  },
   rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
