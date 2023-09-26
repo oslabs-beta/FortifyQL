2623b3da92a0f01ff5993eaa68800296bd510025
Megan Kabat <mnkabat@gmail.com>
2023-09-26 12:34:01 -0700

    added state to config form

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
+import ScanConfigForm from './components/ScanConfigForm';
 import SecurityDashboard from './components/SecurityDashboard';
 
 function App() {
-  return <SecurityDashboard />;
+ return <SecurityDashboard />;
 }
 
 export default App;
