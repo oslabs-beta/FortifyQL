91653150f07d5600ca0209449bb3673b40dcdae5
David Yoon <DYoonity@users.noreply.github.com>
2023-09-26 15:49:09 -0400

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import './App.css';
 import SecurityDashboard from './components/SecurityDashboard';
 
