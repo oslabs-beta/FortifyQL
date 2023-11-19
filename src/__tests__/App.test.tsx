c0bdb8c9c3a8fba634a8c4aea2e760e53f23980e
David Yoon <DYoonity@users.noreply.github.com>
2023-11-18 19:50:48 -0500

    Added jest mock for react-modal

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
+jest.mock('react-modal');
+
 test('Renders main page correctly', async () => {
   render(<App />);
   expect(true).toBeTruthy();
